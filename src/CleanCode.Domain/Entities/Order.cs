31154b1a2e6ab152d668254d99e00f04b2a0d734
Abraao <abraaoblackface@outlook.com>
2024-08-06 12:41:29 -0300

    estrutura inicial

---
+++ src/CleanCode.Domain/Entities/Order.cs
@@ -0,0 +1,18 @@
+﻿using CleanCode.Domain.ValueObjects;
+
+namespace CleanCode.Domain.Entities;
+
+public class Order
+{
+    public Order(string cpf)
+    {
+        Cpf = new Cpf(cpf);
+    }
+
+    public Cpf Cpf { get; set; }
+
+    public int GetTotal()
+    {
+        return 0;
+    }
+}
---
+++ test/CleanCode.Test/OrderTests/OrderTest.cs
@@ -0,0 +1,33 @@
+﻿using CleanCode.Domain.Entities;
+
+namespace CleanCode.Test.OrderTests;
+
+[TestClass]
+public class OrderTest
+{
+
+    [TestMethod]
+    public void Deveria_Criar_Um_Pedido_Com_CPF_Valido()
+    {
+        var cpf = "839.435.452-10";
+        var order = new Order(cpf);
+        var total = order.GetTotal();
+
+        Assert.AreEqual(0, total);
+    }
+
+    [TestMethod]
+    public void Deveria_Tentar_Criar_Um_Pedido_Com_CPF_Invalido()
+    {
+        var cpf = "111.111.111-11";
+
+        try
+        {
+            var order = new Order(cpf);
+        }
+        catch (Exception e)
+        {
+            Assert.AreEqual("Cpf Inválido", e.Message);
+        }
+    }
+}
